786654b7331a82a484f75969d20c68d9be17dad4
Marek Ciborowski <senthi236@gmail.com>
2019-12-16 12:59:58 +0100

    started testing

---
+++ Hotel_Management/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db2a6bf7-fce3-497d-8976-073dfe881bd3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Hotel_Management/Tests/Tests/UserRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using DataAccessLayer;
+using DomainObjects.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Repositories.Repositories;
+using System.Data.Entity;
+
+namespace Tests.Tests
+{
+    public class UserRepositoryTests
+    {
+        private UserRepository userRepository;
+        private DatabaseContext databaseContext;
+
+        public UserRepositoryTests()
+        {
+            this.databaseContext = new DatabaseContext();
+            this.userRepository = new UserRepository(this.databaseContext);
+        }
+
+        public User GetTestUser()
+        {
+            return new User
+            {
+                Address = "Świerkowa 2",
+                City = "Warszawa",
+                Email = "email@wp.pl",
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                Identity = 22,
+                IsConfirmed = false,
+                Login = "jankowalski",
+                Password = "123123",
+                Zipcode = "11-111",
+                RoleId = DomainObjects.Enums.RolesEnum.RegularUser
+            };
+        } 
+
+        [TestMethod]
+        public void AddUser_UserModelPassed_UserAddedSuccessfully()
+        {
+            //Arrange
+            var testUser = GetTestUser();
+
+            var context = new Mock<DatabaseContext>();
+            var dbSetMock = new Mock<DbSet<User>>();
+            context.Setup(x => x.Set<User>()).Returns(dbSetMock.Object);
+            dbSetMock.Setup(x => x.Add(It.IsAny<User>())).Returns(testUser);
+
+            // Act
+            var repository = new UserRepository(context.Object);
+            repository.AddUser(testUser);
+
+            //Assert
+            context.Verify(x => x.Set<User>());
+            dbSetMock.Verify(x => x.Add(It.Is<User>(y => y == testUser)));
+            Assert.IsTrue(2 == 2);
+        }
+
+    }
+}
